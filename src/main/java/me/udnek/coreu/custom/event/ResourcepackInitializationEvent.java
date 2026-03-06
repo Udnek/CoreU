65f5b9aa615bfab6d076aa5f2c205717e54b95bf
udnek <gleb.dudorov@gmail.com>
2026-03-06 13:20:09 +0300

    removed deprecated forced files

--- src/main/java/me/udnek/coreu/custom/event/ResourcepackInitializationEvent.java
+++ src/main/java/me/udnek/coreu/custom/event/ResourcepackInitializationEvent.java
@@ -10,24 +10,14 @@ import java.util.List;
 public class ResourcepackInitializationEvent extends CustomEvent{
 
     protected List<RpJsonFile> files = new ArrayList<>();
-    protected List<RpJsonFile> forced = new ArrayList<>();
 
-    public ResourcepackInitializationEvent(){
-    }
+    public ResourcepackInitializationEvent(){}
 
     public void addFile(RpJsonFile file){
-        files.add(file);
-    }
-    @Deprecated
-    public void forceAddFile(RpJsonFile file){
-        //forced.add(file);
         files.add(file);
     }
 
     public List<RpJsonFile> getFiles() {
         return files;
     }
-
-    @Deprecated
-    public List<RpJsonFile> getForcedFiles() {return forced;}
 }
